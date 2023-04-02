7c39eac0111d5bca655b5b0cec923478f28304ab
WirzRexTon <syriusrexton@hotmail.com>
2023-04-02 22:36:18 +0200

    Upload the sql

---
+++ sql.sql
@@ -0,0 +1,299 @@
+-- Listage de la structure de la base pour legacy
+CREATE DATABASE IF NOT EXISTS `legacy` /*!40100 DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci */;
+USE `legacy`;
+
+-- Listage de la structure de la table legacy. addon_account
+CREATE TABLE IF NOT EXISTS `addon_account` (
+  `name` varchar(60) NOT NULL,
+  `label` varchar(100) NOT NULL,
+  `shared` int(11) NOT NULL,
+  PRIMARY KEY (`name`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+-- Listage des données de la table legacy.addon_account : ~2 rows (environ)
+/*!40000 ALTER TABLE `addon_account` DISABLE KEYS */;
+INSERT INTO `addon_account` (`name`, `label`, `shared`) VALUES
+	('bank_savings', 'Savings account', 0),
+	('caution', 'caution', 0);
+/*!40000 ALTER TABLE `addon_account` ENABLE KEYS */;
+
+-- Listage de la structure de la table legacy. addon_account_data
+CREATE TABLE IF NOT EXISTS `addon_account_data` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `account_name` varchar(100) DEFAULT NULL,
+  `money` int(11) NOT NULL,
+  `owner` varchar(46) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `index_addon_account_data_account_name_owner` (`account_name`,`owner`),
+  KEY `index_addon_account_data_account_name` (`account_name`)
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+-- Listage des données de la table legacy.addon_account_data : ~0 rows (environ)
+/*!40000 ALTER TABLE `addon_account_data` DISABLE KEYS */;
+INSERT INTO `addon_account_data` (`id`, `account_name`, `money`, `owner`) VALUES
+	(9, 'caution', 0, 'char1:624d4828792842ea8a63835f04e742a1316648b3'),
+	(10, 'bank_savings', 0, 'char1:624d4828792842ea8a63835f04e742a1316648b3');
+/*!40000 ALTER TABLE `addon_account_data` ENABLE KEYS */;
+
+-- Listage de la structure de la table legacy. addon_inventory
+CREATE TABLE IF NOT EXISTS `addon_inventory` (
+  `name` varchar(60) NOT NULL,
+  `label` varchar(100) NOT NULL,
+  `shared` int(11) NOT NULL,
+  PRIMARY KEY (`name`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+-- Listage des données de la table legacy.addon_inventory : ~0 rows (environ)
+/*!40000 ALTER TABLE `addon_inventory` DISABLE KEYS */;
+/*!40000 ALTER TABLE `addon_inventory` ENABLE KEYS */;
+
+-- Listage de la structure de la table legacy. addon_inventory_items
+CREATE TABLE IF NOT EXISTS `addon_inventory_items` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `inventory_name` varchar(100) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `count` int(11) NOT NULL,
+  `owner` varchar(46) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `index_addon_inventory_items_inventory_name_name` (`inventory_name`,`name`),
+  KEY `index_addon_inventory_items_inventory_name_name_owner` (`inventory_name`,`name`,`owner`),
+  KEY `index_addon_inventory_inventory_name` (`inventory_name`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+-- Listage des données de la table legacy.addon_inventory_items : ~0 rows (environ)
+/*!40000 ALTER TABLE `addon_inventory_items` DISABLE KEYS */;
+/*!40000 ALTER TABLE `addon_inventory_items` ENABLE KEYS */;
+
+-- Listage de la structure de la table legacy. banking
+CREATE TABLE IF NOT EXISTS `banking` (
+  `identifier` varchar(46) DEFAULT NULL,
+  `type` varchar(50) DEFAULT NULL,
+  `amount` int(64) DEFAULT NULL,
+  `time` bigint(20) DEFAULT NULL,
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `balance` int(11) DEFAULT 0,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- Listage des données de la table legacy.banking : ~0 rows (environ)
+/*!40000 ALTER TABLE `banking` DISABLE KEYS */;
+/*!40000 ALTER TABLE `banking` ENABLE KEYS */;
+
+-- Listage de la structure de la table legacy. billing
+CREATE TABLE IF NOT EXISTS `billing` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `identifier` varchar(46) DEFAULT NULL,
+  `sender` varchar(60) NOT NULL,
+  `target_type` varchar(50) NOT NULL,
+  `target` varchar(40) NOT NULL,
+  `label` varchar(255) NOT NULL,
+  `amount` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+-- Listage des données de la table legacy.billing : ~0 rows (environ)
+/*!40000 ALTER TABLE `billing` DISABLE KEYS */;
+/*!40000 ALTER TABLE `billing` ENABLE KEYS */;
+
+-- Listage de la structure de la table legacy. datastore
+CREATE TABLE IF NOT EXISTS `datastore` (
+  `name` varchar(60) NOT NULL,
+  `label` varchar(100) NOT NULL,
+  `shared` int(11) NOT NULL,
+  PRIMARY KEY (`name`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+-- Listage des données de la table legacy.datastore : ~4 rows (environ)
+/*!40000 ALTER TABLE `datastore` DISABLE KEYS */;
+INSERT INTO `datastore` (`name`, `label`, `shared`) VALUES
+	('user_ears', 'Ears', 0),
+	('user_glasses', 'Glasses', 0),
+	('user_helmet', 'Helmet', 0),
+	('user_mask', 'Mask', 0);
+/*!40000 ALTER TABLE `datastore` ENABLE KEYS */;
+
+-- Listage de la structure de la table legacy. datastore_data
+CREATE TABLE IF NOT EXISTS `datastore_data` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(60) NOT NULL,
+  `owner` varchar(46) DEFAULT NULL,
+  `data` longtext DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `index_datastore_data_name_owner` (`name`,`owner`),
+  KEY `index_datastore_data_name` (`name`)
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+-- Listage des données de la table legacy.datastore_data : ~0 rows (environ)
+/*!40000 ALTER TABLE `datastore_data` DISABLE KEYS */;
+INSERT INTO `datastore_data` (`id`, `name`, `owner`, `data`) VALUES
+	(6, 'user_ears', 'char1:624d4828792842ea8a63835f04e742a1316648b3', '{}'),
+	(7, 'user_mask', 'char1:624d4828792842ea8a63835f04e742a1316648b3', '{}'),
+	(8, 'user_helmet', 'char1:624d4828792842ea8a63835f04e742a1316648b3', '{}'),
+	(9, 'user_glasses', 'char1:624d4828792842ea8a63835f04e742a1316648b3', '{}');
+/*!40000 ALTER TABLE `datastore_data` ENABLE KEYS */;
+
+-- Listage de la structure de la table legacy. items
+CREATE TABLE IF NOT EXISTS `items` (
+  `name` varchar(50) NOT NULL,
+  `label` varchar(50) NOT NULL,
+  `weight` int(11) NOT NULL DEFAULT 1,
+  `rare` tinyint(4) NOT NULL DEFAULT 0,
+  `can_remove` tinyint(4) NOT NULL DEFAULT 1,
+  PRIMARY KEY (`name`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+-- Listage des données de la table legacy.items : ~4 rows (environ)
+/*!40000 ALTER TABLE `items` DISABLE KEYS */;
+INSERT INTO `items` (`name`, `label`, `weight`, `rare`, `can_remove`) VALUES
+	('bread', 'Bread', 1, 0, 1),
+	('phone', 'Phone', 1, 0, 1),
+	('radio', 'Radio', 1, 0, 1),
+	('water', 'Water', 1, 0, 1);
+/*!40000 ALTER TABLE `items` ENABLE KEYS */;
+
+-- Listage de la structure de la table legacy. jobs
+CREATE TABLE IF NOT EXISTS `jobs` (
+  `name` varchar(50) NOT NULL,
+  `label` varchar(50) DEFAULT NULL,
+  `whitelisted` tinyint(1) NOT NULL DEFAULT 0,
+  PRIMARY KEY (`name`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+-- Listage des données de la table legacy.jobs : ~1 rows (environ)
+/*!40000 ALTER TABLE `jobs` DISABLE KEYS */;
+INSERT INTO `jobs` (`name`, `label`, `whitelisted`) VALUES
+	('unemployed', 'Unemployed', 0);
+/*!40000 ALTER TABLE `jobs` ENABLE KEYS */;
+
+-- Listage de la structure de la table legacy. job_grades
+CREATE TABLE IF NOT EXISTS `job_grades` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `job_name` varchar(50) DEFAULT NULL,
+  `grade` int(11) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `label` varchar(50) NOT NULL,
+  `salary` int(11) NOT NULL,
+  `skin_male` longtext NOT NULL,
+  `skin_female` longtext NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=41 DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+-- Listage des données de la table legacy.job_grades : ~1 rows (environ)
+/*!40000 ALTER TABLE `job_grades` DISABLE KEYS */;
+INSERT INTO `job_grades` (`id`, `job_name`, `grade`, `name`, `label`, `salary`, `skin_male`, `skin_female`) VALUES
+	(1, 'unemployed', 0, 'unemployed', 'Unemployed', 200, '{}', '{}');
+/*!40000 ALTER TABLE `job_grades` ENABLE KEYS */;
+
+-- Listage de la structure de la table legacy. licenses
+CREATE TABLE IF NOT EXISTS `licenses` (
+  `type` varchar(60) NOT NULL,
+  `label` varchar(60) NOT NULL,
+  PRIMARY KEY (`type`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+-- Listage des données de la table legacy.licenses : ~6 rows (environ)
+/*!40000 ALTER TABLE `licenses` DISABLE KEYS */;
+INSERT INTO `licenses` (`type`, `label`) VALUES
+	('boat', 'Boat License'),
+	('dmv', 'Driving Permit'),
+	('drive', 'Drivers License'),
+	('drive_bike', 'Motorcycle License'),
+	('drive_truck', 'Commercial Drivers License'),
+	('weapon', 'Weapon License'),
+	('weed_processing', 'Weed Processing License');
+/*!40000 ALTER TABLE `licenses` ENABLE KEYS */;
+
+-- Listage de la structure de la table legacy. multicharacter_slots
+CREATE TABLE IF NOT EXISTS `multicharacter_slots` (
+  `identifier` varchar(46) NOT NULL,
+  `slots` int(11) NOT NULL,
+  PRIMARY KEY (`identifier`) USING BTREE,
+  KEY `slots` (`slots`) USING BTREE
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+-- Listage des données de la table legacy.multicharacter_slots : ~0 rows (environ)
+/*!40000 ALTER TABLE `multicharacter_slots` DISABLE KEYS */;
+/*!40000 ALTER TABLE `multicharacter_slots` ENABLE KEYS */;
+
+-- Listage de la structure de la table legacy. owned_vehicles
+CREATE TABLE IF NOT EXISTS `owned_vehicles` (
+  `owner` varchar(46) DEFAULT NULL,
+  `plate` varchar(12) NOT NULL,
+  `vehicle` longtext DEFAULT NULL,
+  `type` varchar(20) NOT NULL DEFAULT 'car',
+  `job` varchar(20) DEFAULT NULL,
+  `stored` tinyint(4) NOT NULL DEFAULT 0,
+  `parking` varchar(60) DEFAULT NULL,
+  `pound` varchar(60) DEFAULT NULL,
+  `glovebox` longtext DEFAULT NULL,
+  `trunk` longtext DEFAULT NULL,
+  PRIMARY KEY (`plate`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+-- Listage des données de la table legacy.owned_vehicles : ~0 rows (environ)
+/*!40000 ALTER TABLE `owned_vehicles` DISABLE KEYS */;
+/*!40000 ALTER TABLE `owned_vehicles` ENABLE KEYS */;
+
+-- Listage de la structure de la table legacy. society_moneywash
+CREATE TABLE IF NOT EXISTS `society_moneywash` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `identifier` varchar(46) DEFAULT NULL,
+  `society` varchar(60) NOT NULL,
+  `amount` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+-- Listage des données de la table legacy.society_moneywash : ~0 rows (environ)
+/*!40000 ALTER TABLE `society_moneywash` DISABLE KEYS */;
+/*!40000 ALTER TABLE `society_moneywash` ENABLE KEYS */;
+
+-- Listage de la structure de la table legacy. users
+CREATE TABLE IF NOT EXISTS `users` (
+  `identifier` varchar(46) NOT NULL,
+  `accounts` longtext DEFAULT NULL,
+  `group` varchar(50) DEFAULT 'user',
+  `inventory` longtext DEFAULT NULL,
+  `job` varchar(20) DEFAULT 'unemployed',
+  `job_grade` int(11) DEFAULT 0,
+  `job2` varchar(20) DEFAULT 'unemployed',
+  `job2_grade` int(11) DEFAULT 0,
+  `loadout` longtext DEFAULT NULL,
+  `metadata` longtext DEFAULT NULL,
+  `position` longtext DEFAULT NULL,
+  `firstname` varchar(16) DEFAULT NULL,
+  `lastname` varchar(16) DEFAULT NULL,
+  `dateofbirth` varchar(10) DEFAULT NULL,
+  `sex` varchar(1) DEFAULT NULL,
+  `height` int(11) DEFAULT NULL,
+  `skin` longtext DEFAULT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `disabled` tinyint(1) DEFAULT 0,
+  `created_at` timestamp NULL DEFAULT current_timestamp(),
+  `last_seen` timestamp NULL DEFAULT NULL ON UPDATE current_timestamp(),
+  `pincode` int(11) DEFAULT NULL,
+  PRIMARY KEY (`identifier`),
+  UNIQUE KEY `id` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+-- Listage des données de la table legacy.users : ~0 rows (environ)
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` (`identifier`, `accounts`, `group`, `inventory`, `job`, `job_grade`, `job2`, `job2_grade`, `loadout`, `metadata`, `position`, `firstname`, `lastname`, `dateofbirth`, `sex`, `height`, `skin`, `id`, `disabled`, `created_at`, `last_seen`, `pincode`) VALUES
+	('char1:624d4828792842ea8a63835f04e742a1316648b3', '{"bank":50200,"money":0,"black_money":0}', 'admin', '[]', 'unemployed', 0, 'unemployed', 0, '[]', '[]', '{"z":29.229248046875,"y":-1395.072509765625,"x":92.83516693115235,"heading":53.85826873779297}', 'Jhon', 'Aaron', '08/02/1983', 'm', 190, '{"mask_2":0,"chest_3":0,"chain_1":22,"blush_1":0,"eyebrows_4":0,"face_md_weight":50,"bracelets_1":-1,"torso_1":23,"bodyb_2":0,"arms_2":0,"beard_3":0,"watches_1":-1,"bags_2":0,"makeup_3":0,"lipstick_2":0,"chin_1":0,"torso_2":2,"complexion_1":0,"pants_2":3,"blush_2":0,"hair_color_1":3,"tshirt_2":2,"tshirt_1":4,"nose_6":0,"nose_1":0,"shoes_2":2,"nose_2":0,"chin_13":0,"glasses_1":0,"glasses_2":0,"bracelets_2":0,"cheeks_2":0,"age_1":0,"blemishes_2":0,"moles_2":0,"makeup_1":0,"watches_2":0,"lipstick_3":0,"makeup_4":0,"chain_2":2,"eye_squint":0,"hair_2":0,"blemishes_1":0,"makeup_2":0,"nose_5":0,"bodyb_4":0,"jaw_2":0,"nose_3":0,"eyebrows_1":0,"helmet_2":0,"neck_thickness":1,"mask_1":0,"chest_2":0,"eye_color":0,"eyebrows_2":0,"lip_thickness":0,"bags_1":0,"moles_1":0,"decals_2":0,"mom":21,"eyebrows_5":0,"chin_4":0,"pants_1":28,"decals_1":0,"nose_4":0,"sex":0,"lipstick_1":0,"jaw_1":0,"hair_color_2":0,"skin_md_weight":50,"chest_1":0,"ears_1":-1,"ears_2":0,"beard_4":0,"complexion_2":0,"blush_3":0,"cheeks_1":0,"bodyb_1":-1,"helmet_1":-1,"lipstick_4":0,"eyebrows_6":0,"beard_1":11,"sun_2":0,"hair_1":6,"bodyb_3":-1,"age_2":0,"sun_1":0,"arms":1,"cheeks_3":0,"eyebrows_3":0,"bproof_2":0,"chin_2":0,"bproof_1":0,"dad":0,"chin_3":0,"beard_2":10,"shoes_1":70}', 3, 0, '2023-04-02 18:53:22', '2023-04-02 22:11:41', NULL);
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+
+-- Listage de la structure de la table legacy. user_licenses
+CREATE TABLE IF NOT EXISTS `user_licenses` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `type` varchar(60) NOT NULL,
+  `owner` varchar(46) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+-- Listage des données de la table legacy.user_licenses : ~0 rows (environ)
+/*!40000 ALTER TABLE `user_licenses` DISABLE KEYS */;
+/*!40000 ALTER TABLE `user_licenses` ENABLE KEYS */;
+
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
